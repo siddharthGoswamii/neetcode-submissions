a17b7d270b02813c0623606ccca146eaff48bd4b
Siddharth Goswami <155048225+siddharthGoswamii@users.noreply.github.com>
2026-06-17 01:20:31 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int row=0; row<9 ; row++){
+            unordered_set<char> seen;
+            for(int col=0; col<9; col++){
+                if(board[row][col]=='.'){
+                    continue;
+                }
+                if(seen.count(board[row][col])){
+                    return false;
+                }
+                seen.insert(board[row][col]);
+            }
+        }
+
+        for(int col=0; col<9 ; col++){
+            unordered_set<char> seen;
+            for(int row=0; row<9; row++){
+                if(board[row][col]=='.'){
+                    continue;
+                }
+                if(seen.count(board[row][col])){
+                    return false;
+                }
+                seen.insert(board[row][col]);
+                
+            }
+        }
+
+        for(int square=0; square<9; square++){
+            unordered_set<char> seen;
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+
+                    int row = (square / 3)*3+i;
+                    int col = (square % 3)*3+j;
+
+                    if(board[row][col]=='.'){
+                        continue;
+                    }
+
+                    if(seen.count(board[row][col])){
+                        return false;
+                    }
+
+                    seen.insert(board[row][col]);
+                }
+            }
+        }
+        return true;
+    }
+};
