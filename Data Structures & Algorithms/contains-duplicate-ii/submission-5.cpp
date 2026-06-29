1b5e97b53101de75c9fe4ae61ff32accdea1bf0c
Siddharth Goswami <155048225+siddharthGoswamii@users.noreply.github.com>
2026-06-29 18:14:06 +0530

    Add: contains-duplicate-ii - submission-5

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+        map<int,int> res;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(res.count(nums[i])){
+                if((i - res[nums[i]]) <= k) return true;
+            }
+
+            res[nums[i]] = i;
+        }
+        return false;
+
+    }
+};
