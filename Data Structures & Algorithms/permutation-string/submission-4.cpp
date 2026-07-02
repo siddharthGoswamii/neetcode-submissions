630a7906703644a1bf1ccec8fcd3ffff7410e4a2
Siddharth Goswami <155048225+siddharthGoswamii@users.noreply.github.com>
2026-07-02 18:24:25 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> count1;
+        for(char c: s1){
+            count1[c]++;
+        }
+        int n1 = count1.size();
+
+        for(int i = 0 ; i < s2.size() ; i++){
+            unordered_map<char,int> count2;
+            int curr = 0;
+            for(int j = i ; j < s2.size() ; j++){
+                char c = s2[j];
+                count2[c]++;
+
+                if(count1[c] < count2[c]){
+                    break;
+                }
+
+                if(count1[c] == count2[c]){
+                    curr++;
+                }
+
+                if(curr == n1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
