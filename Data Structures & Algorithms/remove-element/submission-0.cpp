1bdf50ecb00e0dcd494dc5ee718fb70383f3410a
Siddharth Goswami <155048225+siddharthGoswamii@users.noreply.github.com>
2026-07-09 00:22:04 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        unordered_map<int,int> mp;
+        int l = 0;
+        for(int num:nums){
+           if(num!=val){
+            nums[l++] = num;
+           }
+        }
+    //    int count = 0;
+    //     for(int i = 0 ; i < mp.size() ; i++){
+    //         while(mp[i] == 0){
+
+    //         }
+    //     }
+        return l;
+    }
+};
