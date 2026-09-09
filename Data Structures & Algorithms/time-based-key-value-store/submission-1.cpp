baad22af31a184ac4e4b67cd2df98442865dbfd2
Siddharth Goswami <155048225+siddharthGoswamii@users.noreply.github.com>
2026-09-10 01:34:22 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,16 @@
+class TimeMap {
+public:
+    unordered_map<string, map<int, string>> mp;
+    TimeMap() {
+        
+    }
+        
+    void set(string key, string value, int timestamp) {
+        mp[key].insert({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+        auto it = mp[key].upper_bound(timestamp);
+        return it == mp[key].begin() ? "" : prev(it)->second;
+    }
+};
